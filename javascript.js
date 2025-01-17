0f2647f118c93a7523bed87a59fcab41a2da2dce
Ethan Cao <ethancao0104@gmail.com>
2025-01-17 14:40:13 -0800

    Implemented the new grid button to allow the user to change the amount of squares in the grid

--- javascript.js
+++ javascript.js
@@ -1,18 +1,19 @@
+//creating a value of the dimension that will be preset at 16
+let dimensionVal = 16;
 
 //create a function that creates a grid of div squares
-function createGrid(rows, columns) {
+function createGrid(dimension) {
   const container = document.querySelector('.container');
   container.innerHTML = '';
 
-  const squareWidth = 800 / columns;
-  const squareHeight = 800 / rows;
+  const squareDimension = 800 / dimension;
 
-  const totalSquares = rows * columns;
+  const totalSquares = dimension * dimension;
   for (let i = 0; i < totalSquares; i++) {
     const div = document.createElement('div');
     div.classList.add('grid-divs');
-    div.style.flex = `${squareWidth}px`;
-    div.style.height = `${squareHeight}px`;
+    div.style.flex = `${squareDimension}px`;
+    div.style.height = `${squareDimension}px`;
     div.addEventListener('mouseover', () => {
       div.classList.add('hovered');
     })
@@ -20,11 +21,23 @@ function createGrid(rows, columns) {
   }
 }
 
+// creating the reset button
 const button = document.querySelector('.reset');
 
 button.addEventListener('click', () => {
   const container = document.querySelector('.container');
-  createGrid(16, 16);
+  createGrid(dimensionVal);
 })
 
-createGrid(16, 16);
+//creating the change dimension button
+const newGrid = document.querySelector('.prompt');
+
+newGrid.addEventListener('click', () => {
+  let newVal = prompt("Please enter a new grid area");
+  dimensionVal = parseInt(newVal, 10);
+  createGrid(dimensionVal);
+})
+
+
+//the grid will be created at default of 16x16
+createGrid(dimensionVal);
